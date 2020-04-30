b96e489f06c14f44034dd7e01a50e6c4638cf4a5
Toopala <>
2020-04-30 13:34:03 +0300

    fix: Review fixes. Script will now print html form to test the calculation. Doc urls point to live documentation

--- python/GenerateE2Authcode.py
+++ python/GenerateE2Authcode.py
@@ -1,16 +1,17 @@
 import hashlib
 import random
+import typing
 
 """
 This is an example how to calculate authcode for Paytrail E2 payment interface.
-More information about authcode can be found in: https://paytraildocs.netlify.app/payments/e2-interface/authcode/
+More information about authcode can be found in: https://docs.paytrail.com/en/ch04s07.html#idm4813351648
 """
 
-def generateParamsIn(paymentParameters) -> str:
+def generateParamsIn(paymentParameters: typing.Dict[str, str]) -> str:
     """
     Generates PARAMS_IN parameter from paymentParameters
     :param paymentParameters:
-    :return:
+    :return paramsIn:
     """
 
     paramsIn = ""
@@ -19,11 +20,10 @@ def generateParamsIn(paymentParameters) -> str:
             paramsIn += f"{key},"
 
     # Remove trailing comma ','
-    paramsIn = paramsIn[:-1]
-    return paramsIn
+    return paramsIn[:-1]
 
 
-def generateAuthcode(paymentParameters) -> str:
+def generateAuthcode(paymentParameters: typing.Dict[str, str]) -> str:
     """
     Generates authcode for E2 payment
     :param paymentParameters:
@@ -40,17 +40,18 @@ def generateAuthcode(paymentParameters) -> str:
     return hashlib.sha256(base64Authcode).hexdigest().upper()
 
 
-merchantId = 1
-merchantHash = ""
+# Demo merchant id and hash
+merchantId = 13466
+merchantHash = "6pKF4jkv97zmqBJ3ZL8gUw5DfT2NMQ"
 amount = 1
 paymentMethods = "1,2"
 
-# See accepted parameters from https://paytraildocs.netlify.app/payments/e2-interface/fields/
+# See accepted parameters from https://docs.paytrail.com/en/ch04s03.html#idm4814369536
 paymentParameters = {
     "MERCHANT_HASH": merchantHash,
     "MERCHANT_ID": merchantId,
-    "URL_SUCCESS": f"https://test.url.com/success",
-    "URL_CANCEL": f"https://test.url.com/cancel",
+    "URL_SUCCESS": "https://test.url.com/success",
+    "URL_CANCEL": "https://test.url.com/cancel",
     "ORDER_NUMBER": f"ORDER-{random.randrange(1, 9000)}",
     "PARAMS_IN": "",
     "PARAMS_OUT": "PAYMENT_ID,TIMESTAMP,STATUS",
@@ -60,4 +61,10 @@ paymentParameters = {
 paymentParameters["PARAMS_IN"] = generateParamsIn(paymentParameters)
 paymentParameters.update({"AUTHCODE": generateAuthcode(paymentParameters)})
 
-print(generateAuthcode(paymentParameters))
+html ='<form action="https://payment.paytrail.com/e2" method="post">\n'
+for key, value in paymentParameters.items():
+    html += f'      <input name="{key}" type="hidden" value="{value}">\n'
+html += '   <input type="submit" value="Pay here">\n</form>'
+
+print("Html form to test:")
+print(html)
