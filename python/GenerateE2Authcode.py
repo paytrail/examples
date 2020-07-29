5a4e7dd4ecde39eae7640dbd55ed1a8a2b4b4ba9
Toopala <>
2020-07-29 10:32:34 +0300

    feat: python example supports item rows

--- python/GenerateE2Authcode.py
+++ python/GenerateE2Authcode.py
@@ -3,8 +3,8 @@ import random
 import typing
 
 """
-This is an example how to calculate authcode for Paytrail E2 payment interface.
-More information about authcode can be found in: https://docs.paytrail.com/en/ch04s07.html#idm4813351648
+This is an example how to calculate AUTHCODE Paytrail E2 payment interface.
+More information about AUTHCODE can be found in: https://docs.paytrail.com/payments/e2-interface/authcode/
 """
 
 def generateParamsIn(paymentParameters: typing.Dict[str, str]) -> str:
@@ -25,7 +25,26 @@ def generateAuthcode(paymentParameters: typing.Dict[str, str]) -> str:
     """
 
     authcodeString = "|".join([str(value) for value in paymentParameters.values()])
-    return hashlib.sha256(b"{authcodeString}").hexdigest().upper()
+    return hashlib.sha256(str.encode(authcodeString)).hexdigest().upper()
+
+def formatItemRows(itemRows) -> dict:
+    """
+    Formats array of item rows to form
+    {
+         "ITEM_TITLE[0]": "test title",
+         "ITEM_TITLE[1]": "test title 2"
+    }
+
+    :param itemRows:
+    :return:
+    """
+
+    items = {}
+    for title, item in itemRows.items():
+        for index, value in enumerate(item):
+           items[f"{title}[{index}]"] = value
+
+    return items
 
 def formatHTML(params: typing.Dict[str, str]) -> None:
     """
@@ -51,7 +70,7 @@ merchantHash = "6pKF4jkv97zmqBJ3ZL8gUw5DfT2NMQ"
 amount = 59
 paymentMethods = "1,2"
 
-# See accepted parameters from https://docs.paytrail.com/en/ch04s03.html#idm4814369536
+# See accepted parameters from https://docs.paytrail.com/payments/e2-interface/validation/
 paymentParameters = {
     "MERCHANT_HASH": merchantHash,
     "MERCHANT_ID": merchantId,
@@ -60,9 +79,72 @@ paymentParameters = {
     "ORDER_NUMBER": f"ORDER-{random.randrange(1, 9000)}",
     "PARAMS_IN": "",
     "PARAMS_OUT": "PAYMENT_ID,TIMESTAMP,STATUS",
-    "AMOUNT": str(amount),
-    "PAYMENT_METHODS": paymentMethods
+    "PAYMENT_METHODS": paymentMethods,
+    "MSG_UI_MERCHANT_PANEL": "Message",
+    "AMOUNT": amount, #Item rows and AMOUNTS shouldn't be provided at the same time, remove this to submit item rows
+    "URL_NOTIFY": "https://test.url.com/notify",
+    "LOCALE": "fi_FI",
+    "CURRENCY": "EUR",
+    "REFERENCE_NUMBER": "1205769750",
+    "PAYER_PERSON_PHONE": "040123123",
+    "PAYER_PERSON_EMAIL": "theodor.tester@testcompany.com",
+    "PAYER_PERSON_FIRSTNAME": "Theodor",
+    "PAYER_PERSON_LASTNAME": "Tester",
+    "PAYER_COMPANY_NAME": "Testcompany",
+    "PAYER_PERSON_ADDR_STREET": "Test street",
+    "PAYER_PERSON_ADDR_POSTAL_CODE": "23131",
+    "PAYER_PERSON_ADDR_TOWN": "Test town",
+    "PAYER_PERSON_ADDR_COUNTRY": "Test country",
+    "VAT_IS_INCLUDED": "0", #This is only used when item rows are provided
+    "ALG": "1"
 }
+
+itemRows = {
+    'ITEM_TITLE':
+        [
+            'Rubber boot',
+            'Basket ball',
+        ],
+    'ITEM_ID':
+        [
+            '100',
+            '101',
+        ],
+    'ITEM_QUANTITY':
+        [
+            '2',
+            '1',
+        ],
+    'ITEM_UNIT_PRICE':
+        [
+            '10',
+            '5',
+        ],
+    'ITEM_VAT_PERCENT':
+        [
+            '24',
+            '24',
+        ],
+    'ITEM_DISCOUNT_PERCENT':
+        [
+            '0',
+            '0',
+        ],
+    'ITEM_TYPE':
+        [
+            '1',
+            '1',
+        ],
+}
+# Discards item rows and VAT_IS_INCLUDED if AMOUNT is present in payment parameters
+if "AMOUNT" in paymentParameters:
+    itemRows = {}
+    del paymentParameters['VAT_IS_INCLUDED'];
+
+itemRows = formatItemRows(itemRows)
+
+# Merge paymentParameters and itemRows
+paymentParameters = {**paymentParameters, **itemRows}
 paymentParameters["PARAMS_IN"] = generateParamsIn(paymentParameters)
 paymentParameters.update({"AUTHCODE": generateAuthcode(paymentParameters)})
 formatHTML(paymentParameters)
